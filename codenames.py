16b2b115d27e1a113cd9b51958ff8ea4ca1ec1d7
T60D <49369887+T60D@users.noreply.github.com>
2020-10-31 21:43:00 -0700

    mostly works

--- codenames.py
+++ codenames.py
@@ -13,15 +13,16 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import tkinter
 from itertools import combinations
+import copy
 
 #Gensime word2vec model
 import gensim
 model = gensim.models.KeyedVectors.load_word2vec_format(
-    'GoogleNews-vectors-negative300.bin', binary=True, limit=500000
+    'GoogleNews-vectors-negative300.bin', binary=True, limit=1000000
 )
 
 #Solving method, either wiki or word2Vec
-solvingMethod = 'word2Vec'
+solvingMethod = 'wiki'
 
 #Additional words that need to be removed
 removedWords = ["besides", "also", "likewise", "too", "aswell", "st", "th", "i", "ane" \
@@ -88,21 +89,22 @@ def getWikiWords(inputWord):
 		content[i] = float((((content[i] - minVal) * (newMax - newMin)) / (maxVal - minVal)) + newMin)
 
 	#Sort and print the dictionary
-	#counter = 0
-	#sorted_content = sorted(content.items(), key=lambda x: x[1], reverse=True)
-	#for i in sorted_content:
-	#	print(i[0], i[1])
-	#	counter += 1
-	#	if counter > 10:
-	#		break
+	counter = 0
+	newContent = dict()
+	sorted_content = sorted(content.items(), key=lambda x: x[1], reverse=True)
+	for i in sorted_content:
+		newContent[i[0]] = i[1]
+		counter += 1
+		if counter > 1000:
+			break
 
-	return content
+	return newContent
 
 #Uses word2Vec to get the most similar words
 def getWord2VecWords(inputWord):
-	#Get the 500 most similar words
+	#Get the 1000 most similar words
 	try:
-		relatedWords = model.similar_by_word(inputWord, 500)
+		relatedWords = model.similar_by_word(inputWord, 1000)
 	except:
 		#If the word was not present in this model, use wiki words
 		return getWikiWords(inputWord)
@@ -254,9 +256,112 @@ class WordConnections:
 			if(counter > 10):
 				break
 
+#Class for storing all the data related for decision making
+class DecisionData():
+	#Initializing the class
+	def __init__(self, connectionWord, numNodes, nodeNames, nodeStrength, \
+		enemyStrength, neutralStrength, blackStrength):
+		#Word that connects the nodes
+		self.connectionWord = connectionWord
+		#Number of nodes that are being connected which is 1-4
+		self.numNodes = numNodes
+		#Names of the nodes being connected, 1-4 values
+		self.nodeNames = nodeNames
+		#Strength of the node to connection word, 1-4 values
+		self.nodeStrength = nodeStrength
+		#Maximum strength between an enemy word and the connection
+		self.maxEnemyStrength = enemyStrength
+		#Maximum strength between a neutral word and the connection
+		self.maxNeutralStrength = neutralStrength
+		#Maximum strength between the black word and the connection
+		self.maxBlackStrength = blackStrength
+
+		#Create the value function
+		#The value is the sum of the node strengths 
+		#If any of the nodeStrengths are less than neutral, enemy, or black
+		#set the value to 0
+		try:
+			self.value = sum(self.nodeStrength)
+			self.minNodeStrength = min(self.nodeStrength)
+		except:
+			self.value = self.nodeStrength
+			self.minNodeStrength = self.nodeStrength
+		
+		strongestEnemy = max([self.maxEnemyStrength, self.maxNeutralStrength, self.maxBlackStrength])
+		if(self.minNodeStrength < strongestEnemy):
+			self.value = 0
+
+#Helper function for sorting a list of decisions
+#Returns the value of a decision
+def getDecisionValue(inputDecision):
+	return inputDecision.value
+
+#Prints out a list of possible decisions given a list of DecisionData
+def printDecisionList(decisionList):
+	for i in decisionList:
+		printDecision(i)
+
+def printDecision(decision):
+	output = decision.connectionWord + " " + str(decision.numNodes) + " [ "
+	for j in decision.nodeNames:
+		output = output + j + " "
+	output = output + "] "
+	output = output + str(decision.value)
+	print(output)
+
+#Stores a decision path through a game, keeping track of the game state
+class DecisionPath():
+	#Initialize the class with the list of team words
+	def __init__(self, teamWordsInput):
+		self.teamWords = copy.deepcopy(teamWordsInput)
+		self.decisionList = []
+
+	#Returns true if the path is complete, there are still words remaining
+	def pathComplete(self):
+		if(self.teamWords):
+			return False
+		return True
+	
+	#Returns the list of team words remaining
+	def getRemainingWords(self):
+		return self.teamWords
+
+	#Adds a new decision to the path
+	def addDecision(self, newDecision):
+		self.decisionList.append(newDecision)
+		#Update the list of team words to remove the ones in the decision
+		for i in newDecision.nodeNames:
+			self.teamWords.remove(i)
+
+	#Removes the most recently added decision to the path
+	def popDecision(self):
+		removedDecision = self.decisionList[-1]
+		#Add the words back to the list
+		for i in removedDecision.nodeNames:
+			self.teamWords.append(i)
+		#Remove the decision
+		self.decisionList.pop()
+
+	#Returns the value
+	def getValue(self):
+		#Update the value of the path which is average of the decision values divided by the path length
+		decisionValues = []
+		for i in self.decisionList:
+			decisionValues.append(i.value)
+		self.value = sum(decisionValues)/len(self.decisionList)/len(self.decisionList)
+		return self.value
+
+	#Prints out the decision path
+	def printDecisionPath(self):
+		#printDecision(self.decisionList[1])
+		printDecisionList(self.decisionList)
+
 class Spymaster():
 	#Initialize the class with all the input words
-	def __init__(self, teamColorInput, redWordsInput, blueWordsInput, neutralWordsInput, blackWordInput):
+	def __init__(self):
+		pass
+
+	def addWords(self, teamColorInput, redWordsInput, blueWordsInput, neutralWordsInput, blackWordInput):
 		print("Beginning game as spymaster")
 		self.teamColor = teamColorInput.lower()
 		if(teamColorInput == "red"):
@@ -271,65 +376,88 @@ class Spymaster():
 		#Initialize the graph
 		self.G = nx.DiGraph()
 
+		print("Calculating word map...")
 		#Add all the words to the graph
 		for i in self.teamWords:
-			print("Calculating map for", i)
+			#print("Calculating map for", i)
 			self.G = addWordToMap(self.G, i, solvingMethod)
 		for i in self.enemyWords:
-			print("Calculating map for", i)
+			#print("Calculating map for", i)
 			self.G = addWordToMap(self.G, i, solvingMethod)
 		for i in self.neutralWords:
-			print("Calculating map for", i)
+			#print("Calculating map for", i)
 			self.G = addWordToMap(self.G, i, solvingMethod)
-		print("Calculating map for", self.blackWord)
+		#print("Calculating map for", self.blackWord)
 		self.G = addWordToMap(self.G, self.blackWord, solvingMethod)
 
 		print("Map of all words created")
 
+	def test(self):
+		wordlist = ["car", "truck", "train"]
+		combConnection = WordConnections(self.G, wordlist)
+		print("Output Combos")
+		print(combConnection.getConnectionNodes())
+
 	#Generates the list of data that shows all of the possible decisions and their strengths
 	def generateDecisionMatrix(self, teamWordsInput, enemyWordsInput, neutralWordsInput, blackWordsInput):
-		print("Start creating decision matrix")
 		decisionList = []
 		#Loop over all the possible connections of words for the team
 		#Calculate for combinations of 2, 3, 4
 		comb = list()
 
 		if(len(teamWordsInput) >= 2):
-			comb.extend(list(combinations(self.teamWords, 2)))
+			comb.extend(list(combinations(teamWordsInput, 2)))
 		if(len(teamWordsInput) >= 3):
-			comb.extend(list(combinations(self.teamWords, 3)))
+			comb.extend(list(combinations(teamWordsInput, 3)))
 		if(len(teamWordsInput) >= 4):
-			comb.extend(list(combinations(self.teamWords, 4)))
+			comb.extend(list(combinations(teamWordsInput, 4)))
 
 		#Where i is the words that need to be combinded over all possible combinations
 		for i in comb:
 			#Generate the possible connections over i
-			combConnection = WordConnections(self.G, i);
+			combConnection = WordConnections(self.G, i)
 			for connectionNode in combConnection.getConnectionNodes():
 				#Get the strenth between the connection word and the others
 				nodeStrength = []
-				for j in i: #i is all the words being connected
-					print(connectionNode, j)
+				for j in i:
 					nodeStrength.append(self.G[j][connectionNode]["weight"])
 
 				#Find the max strength between enemy words and the connector
 				enemyStrength = []
 				for j in enemyWordsInput:
-					enemyStrength.append(self.G[j][connectionNode]["weight"])
+					#If the connection is there, add it
+					if(connectionNode in self.G.neighbors(j)):
+						enemyStrength.append(self.G[j][connectionNode]["weight"])
+					#Otherwise, add a 0
+					else:
+						enemyStrength.append(0)
+				if(not enemyStrength):
+					enemyStrength = [0]
 				#Find the max strength between the neutral words and the connector
 				neutralStrength = []
 				for j in neutralWordsInput:
-					neutralStrength.append(self.G[j][connectionNode]["weight"])
+					#if the connection is there, add it
+					if(connectionNode in self.G.neighbors(j)):
+						neutralStrength.append(self.G[j][connectionNode]["weight"])
+					else:
+						neutralStrength.append(0)
+				if(not neutralStrength):
+					neutralStrength = [0]
 				#Find the black word strength to the connection node
-				blackStrength = self.G[blackWordsInput][connectionNode]["weight"]
+				blackStrength = 0
+				if(connectionNode in self.G.neighbors(blackWordsInput)):
+					blackStrength = self.G[blackWordsInput][connectionNode]["weight"]
 				
 				#Create the decision data and push it to the list
 				newDecisionData = DecisionData(connectionNode, len(i), i, nodeStrength, \
 					max(enemyStrength), max(neutralStrength), blackStrength)
-				decisionList.append(newDecisionData)
+
+				#If the value is nonzero, add it to the list
+				if(newDecisionData.value > 0):
+					decisionList.append(newDecisionData)
 
 		#If there is only one word left, add it to the decision list
-		if(len(teamWordsInput) == 1):
+		if(len(teamWordsInput) == 1 or ((not decisionList) and len(teamWordsInput) < 3)):
 			nodeName = teamWordsInput[0]
 			#Get the most relevant words
 			if(solvingMethod == 'wiki'):
@@ -344,67 +472,252 @@ class Spymaster():
 				#Find the max strength between enemy words and the connector
 				enemyStrength = []
 				for j in enemyWordsInput:
-					enemyStrength.append(self.G[j][i]["weight"])
+					if(nodeName in self.G.neighbors(j)):
+						enemyStrength.append(self.G[j][nodeName]["weight"])
+					else:
+						enemyStrength.append(0)
+				if(not enemyStrength):
+					enemyStrength = [0]
 				#Find the max strength between the neutral words and the connector
 				neutralStrength = []
 				for j in neutralWordsInput:
-					neutralStrength.append(self.G[j][i]["weight"])
+					if(nodeName in self.G.neighbors(j)):
+						neutralStrength.append(self.G[j][nodeName]["weight"])
+					else:
+						neutralStrength.append(0)
+				if(not neutralStrength):
+					neutralStrength = [0]
 				#Find the black word strength to the connection node
-				blackStrength = self.G[blackWordsInput][i]["weight"]
+				blackStrength = 0
+				if(nodeName in self.G.neighbors(blackWordsInput)):
+					blackStrength = self.G[blackWordsInput][nodeName]["weight"]
 
 				#Create the decision data and push it to the list
-				newDecisionData = DecisionData(i, 1, nodeName, nodeStrength, \
+				newDecisionData = DecisionData(i, 1, [nodeName], [nodeStrength], \
 					max(enemyStrength), max(neutralStrength), blackStrength)
-				decisionList.append(newDecisionData)
+				
+				if(newDecisionData.value > 0):
+					decisionList.append(newDecisionData)
 
+		#Sort the decisionList by value
+		decisionList.sort(reverse=True, key=getDecisionValue)
 		return decisionList
 
-	#Class for storing all the data related for decision making
-	class DecisionData():
-		#Initializing the class
-		def __init__(self, connectionWord, numNodes, nodeNames, nodeStrength, \
-			enemyStrength, neutralStrength, blackStrength):
-			#Word that connects the nodes
-			self.connectionWord = connectionWord
-			#Number of nodes that are being connected which is 1-4
-			self.numNodes = numNodes
-			#Names of the nodes being connected, 1-4 values
-			self.nodeNames = nodeNames
-			#Strength of the node to connection word, 1-4 values
-			self.nodeStrength = nodeStrength
-			#Maximum strength between an enemy word and the connection
-			self.maxEnemyStrength = enemyStrength
-			#Maximum strength between a neutral word and the connection
-			self.maxNeutralStrength = neutralStrength
-			#Maximum strength between the black word and the connection
-			self.maxBlackStrength = blackStrength
+	#Returns the decision with the maximal value based on the current state of the game
+	def makeGreedyDecision(self):
+		#Generate list of possible decisions from current game state
+		decisionList = self.generateDecisionMatrix(self.teamWords, self.enemyWords, self.neutralWords, self.blackWord)
+		#The first decision has the highest value
+		decision = decisionList[0]
+		#Print it out
+		printDecision(decision)
+
+	#Instead of only looking at the current game state, this executes a foreward search
+	#to other possible game states
+	#Returns a path where the first decision is what should be made in the current
+	#state and the rest are possible decisions for the future
+	def makeForwardSeekingDecision(self):
+		#Number of actions to be explored, only the top decisions will be explored
+		decisionCount = 5
+
+		#Create a queue of paths to be explored in the tree
+		queue = []
+		#List of finished paths
+		finishedPaths = []
+
+		#Get the starting list of decisions
+		decisionList = self.generateDecisionMatrix(self.teamWords, self.enemyWords, self.neutralWords, self.blackWord)
+		#Extract the best decisions
+		decisionList = decisionList[0:decisionCount]
+		#Add them to the queue
+		for i in decisionList:
+			newPath = DecisionPath(self.teamWords)
+			newPath.addDecision(i)
+			queue.append(newPath)
+
+		#Loop until the queue is empty
+		while(queue):
+			#Read the first path
+			currentPath = queue[0]
+			#Get list of decisions that could be made from this new state
+			newTeamWords = currentPath.getRemainingWords()
+			if(not newTeamWords):
+				del queue[0]
+				continue
+			#Assume there are no enemy words
+			newEnemyWords = []
+			newDecisionList = self.generateDecisionMatrix(newTeamWords, newEnemyWords, self.neutralWords, self.blackWord)
+			newDecisionList = newDecisionList[0:decisionCount]
+			#Create the new paths and add them to the queue
+			for i in newDecisionList:
+				newPath = copy.deepcopy(currentPath)
+				newPath.addDecision(i)
+				#If the path is complete, add it to finished paths
+				if(newPath.pathComplete()):
+					finishedPaths.append(newPath)
+				#Otherwise, add it to the queue
+				else:
+					queue.append(newPath)
+			print(len(queue))
+			#Remove the current element from the queue
+			del queue[0]
+
+		#Now, finished paths should be filled with all the possible valid paths
+		#Find and return the one with the greatest value
+		bestPath = finishedPaths[0]
+		bestValue = finishedPaths[0].getValue()
+		for i in finishedPaths:
+			if(i.getValue() > bestValue):
+				bestPath = i
+				bestValue = i.getValue()
+
+		#Return the final path
+		bestPath.printDecisionPath()
+		return bestPath
+		
+
+def setTeam():
+	invalidInput = True
+	#Enter user input
+	#Get team color
+	while(invalidInput):
+		invalidInput = False
+		team = input('Enter your team color [red] or [blue]: ').lower()
+		if('r' in team):
+			team = "red"
+		elif('b' in team):
+			team = "blue"
+		else:
+			print("Invalid input")
+			invalidInput = True
+
+	return team
+
+def setColorWords(color):
+	if(color == "black"):
+		wordString = input('Enter the black word [a]: ').lower()
+		return wordString
+	else:
+		wordString = input('Enter the '+color+' words, seperated by commas [a,b,c]: ').lower()
+		words= wordString.split(",")
+		return words
+
+def printGameState(teamColor, redWords, blueWords, neutralWords, blackWord):
+	print("-----------Game State-----------")
+	print("Team Color: " + teamColor)
+	line2 = "Red Words: ["
+	for i in range(len(redWords)):
+		line2 = line2 + redWords[i]
+		if i < (len(redWords)-1):
+			line2 = line2 + ","
+	line2 = line2 + "]"
+	print(line2)
+	line3 = "Blue Words: ["
+	for i in range(len(blueWords)):
+		line3 = line3 + blueWords[i]
+		if i < (len(blueWords)-1):
+			line3 = line3 + ","
+	line3 = line3 + "]"
+	print(line3)
+	line4 = "Neutral Words: ["
+	for i in range(len(neutralWords)):
+		line4 = line4 + neutralWords[i]
+		if i < (len(neutralWords)-1):
+			line4 = line4 + ","
+	line4 = line4 + "]"
+	print(line4)
+	print("Black Word: " + blackWord)
 
 
 
 def main():
 
-	#Create a graph of the word map
-	#G = nx.DiGraph()
+	#newGame = Spymaster(team, redWords, blueWords, neutralWords, blackWord)
 
-	#words = ["cat", "dog", "fox"]
-	#for i in words:
-	#	print("Add", i, "to map")
-	#	G = addWordToMap(G, i, solvingMethod)
-	#connectA = WordConnections(G, words)
-	#connectA.printConnections()
+	#newGame.makeForwardSeekingDecision()
+	
+	#Start the game
+	print("_______________________________")
+	print("-----Starting Codenames AI-----")
+	print("===============================")
 
-	team = "red"
-	redWords = ["car", "cross", "racket", "link", "chick", \
-		"oil", "band", "train", "strike"]
-	blueWords = ["cell", "force", "phoenix", "contract", \
-		"capital", "slug", "log", "fish"]
-	neutralWords = ["stream", "plastic", "back", "apple", \
-		"game", "casino", "turkey"]
-	blackWord = "hollywood"
+	state = "init"
+	playGame = True
 
-	newGame = Spymaster(team, redWords, blueWords, neutralWords, blackWord)
+	teamColor = ""
+	redWords = []
+	blueWords  = []
+	neutralWords = []
+	blackWord = ""
+	spymaster = Spymaster()
 
-	print(newGame.generateDecisionMatrix(redWords, blueWords, neutralWords, blackWord))
+	while(playGame):
+
+		#Initialize the game state
+		if(state == "init"):
+			print("Starting New Game--------------")
+			teamColor = setTeam()
+			redWords = setColorWords("red")
+			blueWords = setColorWords("blue")
+			neutralWords = setColorWords("neutral")
+			blackWord = setColorWords("black")
+			#Output the data to make sure it looks alright
+			printGameState(teamColor, redWords, blueWords, neutralWords, blackWord)
+
+			#Check if the data is correct
+			getInput = input('Is this data correct? [y/n]: ').lower()
+			if('y' in getInput):
+				state = "playing"
+				spymaster.addWords(teamColor, redWords, blueWords, neutralWords, blackWord)
+			#If the input is not correct, restart the init
+
+		if(state == "playing"):
+			#Evaluate if the game should keep running or exit
+			if((not redWords) or (not blueWords)):
+				break
+
+			printGameState(teamColor, redWords, blueWords, neutralWords, blackWord)
+			print("----------Choose an action ----------------------------------")
+			print("[gg] Make the best guess with the current game state")
+			print("[fs] Make the best guess while considering future game states")
+			print("[rm word] Remove a word from the game")
+			print("[add word team] Adds a word to a given team")
+			userInput = input().lower()
+			userInput = userInput.split(" ")
+			if(userInput[0] == 'gg'):
+				print("========HINT========")
+				spymaster.makeGreedyDecision()
+				print("====================")
+			elif(userInput[0] == 'fs'):
+				print("========HINT========")
+				spymaster.makeForwardSeekingDecision()
+				print("====================")
+			elif(userInput[0] == 'rm'):
+				removalWord = userInput[1]
+				if(removalWord in redWords):
+					redWords.remove(removalWord)
+				if(removalWord in blueWords):
+					blueWords.remove(removalWord)
+				if(removalWord in neutralWords):
+					neutralWords.remove(removalWord)
+			elif(userInput[0] == 'add'):
+				teamtoadd = userInput[2]
+				if(teamtoadd == "red"):
+					redWords.append(userInput[1])
+				elif(teamtoadd == "blue"):
+					blueWords.append(userInput[1])
+				elif(teamtoadd == "neutral"):
+					neutralWords.append(userInput[1])
+				else:
+					print("Invalid input")
+			else:
+				print("Invalid input entered")
+
+	print("Thanks for playing")
+	
+	
+	
+
 
 if __name__ == "__main__":
 	main()
